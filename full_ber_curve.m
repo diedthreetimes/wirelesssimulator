464a643eef93a9d520adc4ecaaa239023a71184e
Sky Faber <sky.faber@gmail.com>
2012-12-05 02:35:00 -0800

    working soft decision. ebno buisness. and problem 4

--- full_ber_curve.m
+++ full_ber_curve.m
@@ -1,19 +1,14 @@
-function [ ebnos, bers ] = full_ber_curve( code, ray, dbpsk)
+function [ ebnos, bers ] = full_ber_curve( code, ray, tit)
 %FULL_BER_CURVE Calculate and plot BER Curve
 %   Will run the simulation until 1e-5 error rate is achieved
 %   link_layer_simulator must be opened
 
-global trellis use_rayleigh use_dbpsk m ebno no_csi use_soft no_interleave;
+global trellis use_rayleigh use_dbpsk m ebno no_csi use_soft no_interleave rand_seed;
 
 if nargin < 2
     use_rayleigh = 0;
 else
     use_rayleigh = ray
-end
-if nargin < 3
-    use_dbpsk = 0;
-else
-    use_dbpsk = dbpsk
 end  
 
 m = floor(log2(max(code)))+1;
@@ -22,35 +17,52 @@ ebnos = [];
 bers = [];
 ebno = 0;
 ber = 1;
-max_ebno = 50;
+max_ebno = 100;
+
 trellis = poly2trellis(m, code);
 
-while ber > 1e-5 && ebno < max_ebno    
+detailed = 1;
+if detailed
+    ebno = 28;
+    max_ebno = 32;
+end
+
+while (detailed || ber > 1e-5) && ebno <= max_ebno    
     [t,x,y] = sim('link_layer_simulator');
     
     ber = y(1);
     bers = [bers ber];
     ebnos = [ebnos ebno];
     
+    rand_seed = randi(1000000,1);
+    
     % As we approach the desired ber value,
     % we need to increment ebno by smaller
     % values or we will hit ber=0 and then
     % Matlab can't plot it since it's on a
     % log scale.
-    if ber < 5e-3
-        ebno = ebno + 0.3;
-    elseif ber < 1e-2
-        ebno = ebno + 0.5;
-    elseif ber < 1e-1
-        ebno = ebno + 1;
+    if detailed
+        ebno = ebno + 0.25;
     else
-        ebno = ebno + 5;
+        if ber < 5e-3
+            ebno = ebno + 0.3;
+        elseif ber < 1e-2
+            ebno = ebno + 0.5;
+        elseif ber < 1e-1
+            ebno = ebno + 1;
+        else
+            ebno = ebno + 5;
+        end
     end
 end
 
 
 semilogy(ebnos,bers,'-.or'); %hold on;
-title(['Performance for ' int2str(code)]);
+if(nargin < 3)
+    title(['Performance for ' int2str(code)]);
+else
+    title(tit);
+end
 xlabel('E_b/N_0');
 ylabel('Bit Error Rate');
 
--- main.m
+++ main.m
@@ -1,21 +1,88 @@
 open_system('link_layer_simulator');
 
-global trellis use_rayleigh use_dbpsk m ebno no_csi use_soft no_interleave rand_seed;
+global trellis use_rayleigh use_dbpsk m ebno no_csi use_soft no_interleave rand_seed doppler_shift;
 
 % R inverse (the rate of expansion of bits to coded bits)
 n = 2;
 % The number of look back bits
-m = 2;
+m = 3;
 no_csi = 1;
 use_soft = 0;
 no_interleave = 0;
+use_dbpsk = 0;
 
-per = all_codes(m, n);
+slow_vehicle = 1;
+if slow_vehicle
+    doppler_shift = 2.5;
+else 
+    doppler_shift = 100;
+end
 
-for i = 1:length(per)
-    rand_seed = randi(1000000,1);
+command = 1;
+
+
+% Do Search
+if command == 1
+    per = all_codes(m, n);
+
+    
+    %for i = 1:length(per)
+        figure
+        %[ebnos, bers] = full_ber_curve( per(i,:) )
+        [ebnos, bers] = full_ber_curve( [5 7] )
+    %end
+% Problem 4
+elseif command == 2
+    code = [5 7];
+    
+    m = 3;
+    no_csi = 1;
+    use_soft = 0;
+    no_interleave = 0;
+    use_dbpsk = 0;
+    doppler_shift = 2.5;
+    
+    mkdir 'problem4/awgn';
+    use_soft = 0;
     figure
-    full_ber_curve( per(i,:) )
+    full_ber_curve( code, 0, 'HDD over AWGN')
+    saveas(gcf, 'problem4/awgn/hdd','fig');
+       
+    use_soft = 1;
+    figure
+    full_ber_curve( code, 0, 'SDD over AWGN')
+    saveas(gcf, 'problem4/awgn/sdd','fig');
+    
+    % With and without csi
+    mkdir 'problem4/ray';
+    no_csi = 0;
+    use_soft = 0;
+    figure
+    full_ber_curve( code, 1, 'HDD +CSI over flat rayleigh')
+    saveas(gcf, 'problem4/ray/hdd+csi', 'fig');
+    
+    use_soft = 1;
+    figure
+    full_ber_curve( code, 1, 'SDD +CSI over flat rayleigh')
+    saveas(gcf, 'problem4/ray/sdd+csi','fig');
+    
+    no_csi = 1;
+    use_soft = 0;
+    figure
+    full_ber_curve( code, 1, 'HDD -CSI over flat rayleigh')
+    saveas(gcf, 'problem4/ray/hdd-csi','fig');
+    
+    use_soft = 1;
+    figure
+    full_ber_curve( code, 1, 'SDD -CSI over flat rayleigh')
+    saveas(gcf, 'problem4/ray/sdd-csi','fig');
+    
+    use_soft = 0;
+    no_interleave = 1;
+    no_csi = 1;
+    figure
+    full_ber_curve( code, 1, 'HDD -CSI no interleaving over flat rayleigh')
+    saveas(gcf, 'problem4/ray/hdd-csi-intr','fig');
 end
 
 
