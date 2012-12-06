92804e6352dcaa56a6d4d133f2af91c3d470381b
Sky Faber <sky.faber@gmail.com>
2012-12-05 23:51:42 -0800

    fig2pdf convertion utils

---
+++ figstopdf.m
@@ -0,0 +1,16 @@
+% Convert all figures to pdf
+
+files = getAllFiles('problem4')
+
+for i=1:length(files)
+    
+   % Is this a figure?
+   a = regexpi(files(i), '.*\.fig') ;
+   if a{1} == 1
+    file = files(i);
+    file = file{1};
+    open(file);
+    saveas(gcf, file(1:length(file)-4), 'pdf');
+    close gcf;
+   end
+end
