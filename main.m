fc2aca111e641fd6605f14c63834c385d01d34f0
Sky Faber <sky.faber@gmail.com>
2012-12-06 19:06:27 -0800

    Comments and dbpsk figs

--- main.m
+++ main.m
@@ -2,31 +2,46 @@ open_system('link_layer_simulator');
 
 global trellis use_rayleigh use_dbpsk m n ebno no_csi use_soft no_interleave rand_seed doppler_shift;
 
-% R inverse (the rate of expansion of bits to coded bits)
-n = 2;
-% The number of look back bits
-m = 3;
-no_csi = 1;
-use_soft = 0;
-no_interleave = 0;
-use_dbpsk = 1;
 
-slow_vehicle = 1;
+%%%%%%%%%%%%%%%%%%%%%   Configuration Paramaters  %%%%%%%%%%%%%%%%%%%%
+n = 2;             % R inverse (the rate of expansion of bits to coded bits)
+m = 3;             % The number of look back bits (memory registers)
+no_csi = 1;        % Turn Channel State Information off
+use_soft = 0;      % Use soft decision decoding (instaed of hard decision)
+no_interleave = 0; % Turn off block interleaving
+use_dbpsk = 0;     % Use dbpsk instead of bpsk modulation
+rayleigh = 1;      % Use flat rayleigh fading in addition to AWGN
+code = [5 7];      % A convolution code (matching m and n) specified in decimal 
+slow_vehicle = 1;  % Vehicle speed. Slow is 3km/h. Not slow is 120km/h 
+
+%Commands
+% 1: Run the search algorithim with above settings
+% 2: Run problem 4. The above settings don't affect it
+% 3: Plot a single ber curve with the above settings
+command = 1;
+
+%Command specific paramaters
+
+% Search space commands (will search all combinations
+ms = [3; 4];  % Which m's to search 
+ns = [2; 3];  % Which n's to search
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+
+
+% Doppler shifts are calculated using a 900MHz carrier frequency
+%  These could easily be calculated on the fly
 if slow_vehicle
     doppler_shift = 2.5;
 else 
     doppler_shift = 100;
 end
 
-command = 1;
-
 
 %% Do Search
-if command == 1
-    rayleigh = 1;
-   
-    ms = [2]
-    ns = [2; 3]
+if command == 1   
     for k = 1:length(ms)
         m = ms(k)
         for p = 1:length(ns)
@@ -39,10 +54,8 @@ if command == 1
             min_ber = ones(num_best,1).'*0.5;
             best_per = ones(num_best,n)*-1;
 
-
+            figure; hold off;
             for i = 1:size(per,1)
-                figure;
-                  
                 [ebnos, bers] = full_ber_curve( per(i,:), rayleigh );
                 target_ebno = 999;
                 target_ber = 1;
@@ -69,10 +82,8 @@ if command == 1
                     min_ber(ind) = target_ber;
                     best_per(ind,:) = per(i,:);
                 end
-                
-                close gcf;
             end
-           
+            close gcf;
             
             figure;
             labels = {};
@@ -195,6 +206,8 @@ elseif command == 2
     legend('Low Mobility', 'High Mobility')
     saveas(gcf, 'problem4/ray/dbpsk-mobility', 'fig')
     
+elseif command == 3
+    full_ber_curve(code, rayleigh);
 end
 
 
